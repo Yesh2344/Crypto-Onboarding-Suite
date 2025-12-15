97ffa4ea57acd887eda658c8b7c231a9ac7c9f38
91964 <yeswanthsoma83@gmail.com>
2025-12-14 17:12:38 -0800

    Improve README.md

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
+# Added comment
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
